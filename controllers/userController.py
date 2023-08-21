69f7474767082db515563a9ee1642e8d7ba87c20
leehyeokjun <leehyeokjun@askstory.com>
2023-08-21 11:05:33 +0900

    [POST] /users/signup - 비밀번호 암호화, req.body validator 처리

--- controllers/userController.py
+++ controllers/userController.py
@@ -2,7 +2,11 @@ from models import Users
 from schemas import userSchema
 from sqlalchemy.orm import Session
 from datetime import datetime
+from passlib.context import CryptContext
 import uuid
+
+def encryptedPassword(plainPassword):
+    return CryptContext(schemes=["bcrypt"], deprecated="auto").hash(plainPassword)
 
 def getUserList(db: Session):
     userList = db.query(Users).order_by((Users.created_at.desc())).all()
@@ -12,10 +16,9 @@ def createUser(db: Session, request_data: userSchema.createUserSchema):
     createUserQuery = Users(
         user_id = str(uuid.uuid4()),
         fullname = request_data.fullname,
-        password = request_data.password,
+        password = encryptedPassword(request_data.password),
         email = request_data.email,
         created_at = datetime.now(),
     )
-    print('createUserQuery:', createUserQuery)
     db.add(createUserQuery)
-    db.commit()
+    db.commit() 
--- schemas/userSchema.py
+++ schemas/userSchema.py
@@ -1,15 +1,24 @@
 import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, EmailStr
 
 class getUserInfoSchema(BaseModel):
     user_id: str
     fullname: str
     email: str
 
+    class Config:
+        orm_mode = True
+
 class createUserSchema(BaseModel):
     fullname: str
     password: str
-    email: str
+    email: EmailStr
+
+    @validator('fullname', 'password', 'email')
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError(f'{v} is empty.')
+        return v
 
     class Config:
         orm_mode = True
