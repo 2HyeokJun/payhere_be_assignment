95e8f682d031d4bd85517320e3194370b41ce3ec
leehyeokjun <leehyeokjun@askstory.com>
2023-08-21 14:01:57 +0900

    boards 게시판 모델 작성

--- models.py
+++ models.py
@@ -1,12 +1,22 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from database import Base
 
 class Users(Base):
     __tablename__ = 'users'
 
-    user_id = Column(String, primary_key = True)
+    user_uuid = Column(String, primary_key = True)
     fullname = Column(String, nullable = False)
     password = Column(String, nullable = False)
     email = Column(String, unique = True, nullable = False)
     created_at = Column(DateTime, nullable = False, server_default = func.now())
+
+class Board(Base):
+    __tablename__ = 'boards'
+
+    board_id = Column(Integer, primary_key = True, autoincrement = True)
+    name = Column(String(255), unique = True, nullable = False)
+    public = Column(Boolean, nullable = False)
+    creator_id = Column(String, ForeignKey('users.user_uuid'))
+    creator = relationship('Users')
