7ac484473e5036a0f5acea7e67bb3e2c99e1f2cb
leehyeokjun <leehyeokjun@askstory.com>
2023-08-22 15:52:34 +0900

    코드 간격 수정

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 from routers import userRouter, boardRouter, postRouter
 
-
 app = FastAPI()
 app.include_router(userRouter.router)
 app.include_router(boardRouter.router)
 app.include_router(postRouter.router)
-
-
 
 @app.get('/')
 async def root():
